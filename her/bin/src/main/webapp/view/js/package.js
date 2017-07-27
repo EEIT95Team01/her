50cf91ac3e1953a4056f3e3f6e2586a6258c1625
洪敏睿 <rick199006@gmail.com>
2017-07-27 17:27:26 +0800

    delete

--- her/bin/src/main/webapp/view/js/package.js
+++
@@ -1,4 +0,0 @@
-﻿$(function () {
-    $("#PackageDateMailed").datepicker({ minDate: +10 }).datepicker("option", "changeMonth", true).datepicker("option", "changeYear", true).datepicker("option", "dateFormat", "yy-mm-dd");
-    
-});
