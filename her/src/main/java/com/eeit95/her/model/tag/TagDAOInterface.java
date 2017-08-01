547b4517a5878830dc2456d320c024ec78318850
廖冠皇 <b00303037@gmail.com>
2017-08-01 17:40:44 +0800

    Chage Directory Structure
    tag - tag
    card - card, card_description, card_tag
    font - writer, font, font_description, font_tag
    gift - category_main, category, gift, gift_description, gift_tag
    member - member, repient, history, collection, message
    pack - pack_status, pack, pack_gift, orderform, orderform_pack_status, orderform_pack
    advertisement - advertisement
    admin - authority, admin, operation, admin_log

---
+++ her/src/main/java/com/eeit95/her/model/tag/TagDAOInterface.java
@@ -0,0 +1,14 @@
+package com.eeit95.her.model.tag.dao;
+
+import java.util.List;
+
+import com.eeit95.her.model.card.CardBean;
+import com.eeit95.her.model.tag.TagBean;
+
+public interface TagDAOinterface {
+	public abstract TagBean selectById(int id);
+	public abstract TagBean insert(TagBean bean);
+	public abstract TagBean update(TagBean bean);
+	public abstract boolean delete(int id);
+	public abstract List <TagBean> selectAll();
+}
