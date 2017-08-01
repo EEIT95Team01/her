b2877f754021a05b263a1fbbabd127577d8922dd
廖冠皇 <b00303037@gmail.com>
2017-08-01 12:25:02 +0800

    Add htmls

---
+++ her/src/main/webapp/js/package.js
@@ -0,0 +1,4 @@
+﻿$(function () {
+    $("#DateMailed").datepicker({ minDate: +10 }).datepicker("option", "changeMonth", true).datepicker("option", "changeYear", true).datepicker("option", "dateFormat", "yy-mm-dd");
+    
+});
